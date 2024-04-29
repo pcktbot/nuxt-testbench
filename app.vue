a34289a8920130ce1c0016420949958c687fb0da
pcktbot <pcktbot@gmail.com>
2024-04-29 12:17:08 +0000

    booted

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
-    <h1>Well, hello there!</h1>
+    <CardComponent>
+      <h1>Well, hello there!</h1>
+      <button class="btn">Press To Begin</button>
+    </CardComponent>
   </div>
 </template>
